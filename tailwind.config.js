2fd3f3283b47fb1beac5e14a7cbcb117fe90facd
Gennadii Zemliakov <zemliakov@protonmail.com>
2024-08-09 14:47:27 +0400

    Connect flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+import flowbite from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite
+  ],
 }
